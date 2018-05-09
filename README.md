fef8aac9495c0ea399ae7b8fc4df7673bcdbea48
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2018-05-10 03:47:40 +0900

    First release

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # erb-lint-docker
 A docker image for https://github.com/Shopify/erb-lint
+
+### Usage
+
+ ```bash
+ docker run --rm -ti -v "$(pwd):/workdir" announced/erb-lint --help
+ ```
---
+++ script/el.bash
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+function el() {
+  set -u
+  readonly TAG="announced/erb-lint"
+
+  init() {
+    init-dependencies
+  }
+
+  init-dependencies () {
+    local DEPENDENCIES=(
+      "docker"
+    )
+    for TARGET in "${DEPENDENCIES[@]}"; do
+      if [[ ! -x "$(command -v "${TARGET}")" ]]; then
+      error "Install ${TARGET}."
+    fi
+    done
+  }
+
+  error () {
+    MESSAGE="${1:-'Something went wrong.'}"
+    echo "[$(basename "$0")] ERROR: ${MESSAGE}" >&2
+    exit 1
+  }
+
+  lint () {
+    lint-dockerfile \
+    && lint-shell
+  }
+
+  lint-dockerfile () {
+    docker run --rm -i hadolint/hadolint < Dockerfile
+  }
+
+  lint-shell () {
+    docker run --rm -v "$PWD:/mnt" koalaman/shellcheck:stable script/*.bash
+  }
+
+  build () {
+    docker build -t "${TAG}" .
+  }
+
+  run () {
+    docker run --rm -ti -v "$(pwd):/workdir" "${TAG}" --version
+  }
+
+  release () {
+    lint \
+    && build \
+    && docker push "${TAG}"
+  }
+
+  clean () {
+    docker system prune
+  }
+
+  usage () {
+    SELF="$(basename "$0")"
+    echo -e "${SELF} -- elb-lint-docker
+    \nUsage: ${SELF} [arguments]
+    \nArguments:"
+    declare -F | awk '{print "\t" $3}' | grep -v "${SELF}"
+  }
+
+  if [ $# = 0 ]; then
+    usage
+  elif [ "$(type -t "$1")" = "function" ]; then
+    $1
+  else
+    usage
+  fi
+}
+
+el "$@"
